a20d651e2a9ce762d0956dd65c5d526253acf517
Seongbeom Park <sparkamita90@gmail.com>
2022-08-02 15:58:02 +0900

    feature: implement checkbox of datatable

--- src/component/data_table.js
+++ src/component/data_table.js
@@ -1,8 +1,6 @@
 import { LitElement, html, unsafeCSS } from 'lit';
 import { MDCDataTable } from '@material/data-table';
 
-import '@component/checkbox';
-
 class DataTable extends LitElement {
     static properties = {
         columns: {type: Object},
@@ -15,17 +13,46 @@ class DataTable extends LitElement {
         this.columns = [];
         this.contents = [];
     }
-    createHeader () {
-        const createHeaderCheckbox = () => {
-            if (this.create_checkbox) {
-                return html`
-                    <th class="mdc-data-table__header-cell mdc-data-table__header-cell--checkbox" role="columnheader" scope="col">
-                        <lm-checkbox class="mdc-data-table__header-row-checkbox mdc-checkbox--selected" aria_label="Toggle all rows"></lm-checkbox>
-                    </th>
-                `;
-            }
+    createCheckbox (position, id) {
+        const createContents = ({ class_list, aria_label, aria_labelledby }) => {
+            const _aria_label = aria_label ? `aria-label="${aria_label}"` : ``;
+            const _aria_labelledby = aria_labelledby ? `aria-labelledby="${aria_labelledby}"` : ``;
+            return html`
+                <div class="mdc-checkbox ${unsafeCSS(class_list.join(' '))}">
+                    <input type="checkbox" class="mdc-checkbox__native-control" ${unsafeCSS(_aria_label)} ${unsafeCSS(_aria_labelledby)}/>
+                    <div class="mdc-checkbox__background">
+                        <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
+                            <path class="mdc-checkbox__checkmark-path" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59" />
+                        </svg>
+                        <div class="mdc-checkbox__mixedmark"></div>
+                    </div>
+                    <div class="mdc-checkbox__ripple"></div>
+                </div>
+            `;
         }
 
+        switch (position) {
+            case 'header':
+                return html`
+                    <th class="mdc-data-table__header-cell mdc-data-table__header-cell--checkbox" role="columnheader" scope="col">
+                        ${createContents({
+                            class_list: ["mdc-data-table__header-row-checkbox", "mdc-checkbox--selected"],
+                            aria_label: "Toggle all rows",
+                        })}
+                    </th>
+                `;
+            case 'body':
+                return html`
+                    <td class="mdc-data-table__cell mdc-data-table__cell--checkbox">
+                        ${createContents({
+                            class_list: ["mdc-data-table__row-checkbox"],
+                            aria_labelledby: id
+                        })}
+                    </td>
+                `;
+        }
+    }
+    createHeader () {
         const createHeaderCell = (columns) => {
             return columns.map((column) => html`
                 <th class="mdc-data-table__header-cell" role="columnheader" scope="col">${column}</th>
@@ -35,36 +62,13 @@ class DataTable extends LitElement {
         return html`
             <thead>
                 <tr class="mdc-data-table__header-row">
-
-                    <th class="mdc-data-table__header-cell mdc-data-table__header-cell--checkbox" role="columnheader" scope="col">
-                        <div class="mdc-checkbox mdc-data-table__header-row-checkbox mdc-checkbox--selected">
-                            <input type="checkbox" class="mdc-checkbox__native-control" aria-label="Toggle all rows"/>
-                            <div class="mdc-checkbox__background">
-                                <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
-                                    <path class="mdc-checkbox__checkmark-path" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59" />
-                                </svg>
-                                <div class="mdc-checkbox__mixedmark"></div>
-                            </div>
-                            <div class="mdc-checkbox__ripple"></div>
-                        </div>
-                    </th>
-
+                    ${this.createCheckbox('header')}
                     ${createHeaderCell(this.columns)}
                 </tr>
             </thead>
         `;
     }
     createBody () {
-        const createBodyCheckbox = (id) => {
-            if (this.create_checkbox) {
-                return html`
-                    <td class="mdc-data-table__cell mdc-data-table__cell--checkbox">
-                        <lm-checkbox aria_labelledby="${unsafeCSS(id)}"></lm-checkbox>
-                    </td>
-                `;
-            }
-        }
-
         const createBodyCell = (is_first, id, value) => {
             if (is_first) return html`<th class="mdc-data-table__cell" scope="row" id="${unsafeCSS(id)}">${value}</th>`;
             else return html`<td class="mdc-data-table__cell">${value}</td>`;
@@ -73,26 +77,7 @@ class DataTable extends LitElement {
         const createBodyRow = (id, data) => {
             return html`
                 <tr data-row-id="${unsafeCSS(id)}" class="mdc-data-table__row" >
-
-                    <td class="mdc-data-table__cell mdc-data-table__cell--checkbox">
-                        <div class="mdc-checkbox mdc-data-table__row-checkbox">
-                            <input type="checkbox" class="mdc-checkbox__native-control" aria-labelledby="${unsafeCSS(id)}"/>
-                            <div class="mdc-checkbox__background">
-                                <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
-                                    <path class="mdc-checkbox__checkmark-path" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59" />
-                                </svg>
-                                <div class="mdc-checkbox__mixedmark"></div>
-                            </div>
-                            <div class="mdc-checkbox__ripple"></div>
-                        </div>
-                    </td>
-
-                    ${data.map((value, index) => createBodyCell(index === 0, id, value))}
-                </tr>
-            `;
-            return html`
-                <tr data-row-id="${unsafeCSS(id)}" class="mdc-data-table__row" >
-                    ${createBodyCheckbox(id)}
+                    ${this.createCheckbox('body', id)}
                     ${data.map((value, index) => createBodyCell(index === 0, id, value))}
                 </tr>
             `;
