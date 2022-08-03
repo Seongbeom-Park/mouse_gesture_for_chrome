bbab6d3d0bcd24104663380e8f765d860d7b2171
Seongbeom Park <sparkamita90@gmail.com>
2022-08-04 01:50:50 +0900

    refactor: remove unnecessary global functions of data table

--- src/component/data_table.js
+++ src/component/data_table.js
@@ -16,83 +16,85 @@ export class DataTable extends LitElement {
         this.contents = contents;
         this.rows = {};
     }
-    createCheckbox (position, id) {
-        const createContents = ({ class_list, aria_label, aria_labelledby }) => {
-            return html`
-                <div class="mdc-checkbox ${class_list.join(' ')}">
-                    <input type="checkbox" class="mdc-checkbox__native-control" aria-label=${ifDefined(aria_label)} aria-labelledby=${ifDefined(aria_labelledby)}/>
-                    <div class="mdc-checkbox__background">
-                        <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
-                            <path class="mdc-checkbox__checkmark-path" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59" />
-                        </svg>
-                        <div class="mdc-checkbox__mixedmark"></div>
-                    </div>
-                    <div class="mdc-checkbox__ripple"></div>
-                </div>
-            `;
-        }
-
-        return html`
-            ${choose(position, [
-                ['header', () => html`
-                    <th class="mdc-data-table__header-cell mdc-data-table__header-cell--checkbox" role="columnheader" scope="col">
-                        ${createContents({
-                            class_list: ["mdc-data-table__header-row-checkbox", "mdc-checkbox--selected"],
-                            aria_label: "Toggle all rows",
-                        })}
-                    </th>
-                `],
-                ['body', () => html`
-                    <td class="mdc-data-table__cell mdc-data-table__cell--checkbox">
-                        ${createContents({
-                            class_list: ["mdc-data-table__row-checkbox"],
-                            aria_labelledby: id
-                        })}
-                    </td>
-                `],
-            ])}
-        `;
-    }
-    createHeader () {
-        const createHeaderCell = (column) => html`<th class="mdc-data-table__header-cell" role="columnheader" scope="col">${column}</th>`;
-
-        return html`
-            <thead>
-                <tr class="mdc-data-table__header-row">
-                    ${this.createCheckbox('header')}
-                    ${map(this.columns, createHeaderCell)}
-                </tr>
-            </thead>
-        `;
-    }
-    createBody () {
-        const createBodyCell = ({scope, id, value}) => {
-            return html`<th class="mdc-data-table__cell" scope=${ifDefined(scope)} id=${ifDefined(id)}>${unsafeHTML(value)}</th>`;
-        }
-
-        const createBodyRow = (id, data) => {
-            this.rows[id] = data;
-            return html`
-                <tr data-row-id=${id} class="mdc-data-table__row" >
-                    ${this.createCheckbox('body', id)}
-                    ${map(data, (value, index) => createBodyCell(index === 0 ? {scope: 'row', id, value} : {value}))}
-                </tr>
-            `;
-        }
-
-        return html`
-            <tbody class="mdc-data-table__content">
-                ${map(this.contents, (content, index) => createBodyRow(`${this.id}_row_${index}`, content))}
-            </tbody>
-        `;
-    }
     render () {
-        const aria_label = this.ariaLabel ?? "Data Table";
+        const createCheckbox = (position, id) => {
+            const createContents = ({ class_list, aria_label, aria_labelledby }) => {
+                return html`
+                    <div class="mdc-checkbox ${class_list.join(' ')}">
+                        <input type="checkbox" class="mdc-checkbox__native-control" aria-label=${ifDefined(aria_label)} aria-labelledby=${ifDefined(aria_labelledby)}/>
+                        <div class="mdc-checkbox__background">
+                            <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
+                                <path class="mdc-checkbox__checkmark-path" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59" />
+                            </svg>
+                            <div class="mdc-checkbox__mixedmark"></div>
+                        </div>
+                        <div class="mdc-checkbox__ripple"></div>
+                    </div>
+                `;
+            }
+    
+            return html`
+                ${choose(position, [
+                    ['header', () => html`
+                        <th class="mdc-data-table__header-cell mdc-data-table__header-cell--checkbox" role="columnheader" scope="col">
+                            ${createContents({
+                                class_list: ["mdc-data-table__header-row-checkbox", "mdc-checkbox--selected"],
+                                aria_label: "Toggle all rows",
+                            })}
+                        </th>
+                    `],
+                    ['body', () => html`
+                        <td class="mdc-data-table__cell mdc-data-table__cell--checkbox">
+                            ${createContents({
+                                class_list: ["mdc-data-table__row-checkbox"],
+                                aria_labelledby: id
+                            })}
+                        </td>
+                    `],
+                ])}
+            `;
+        }
+
+        const createHeader = () => {
+            const createHeaderCell = (column) => html`<th class="mdc-data-table__header-cell" role="columnheader" scope="col">${column}</th>`;
+    
+            return html`
+                <thead>
+                    <tr class="mdc-data-table__header-row">
+                        ${createCheckbox('header')}
+                        ${map(this.columns, createHeaderCell)}
+                    </tr>
+                </thead>
+            `;
+        }
+        
+        const createBody = () => {
+            const createBodyCell = ({scope, id, value}) => {
+                return html`<th class="mdc-data-table__cell" scope=${ifDefined(scope)} id=${ifDefined(id)}>${unsafeHTML(value)}</th>`;
+            }
+
+            const createBodyRow = (id, data) => {
+                this.rows[id] = data;
+                return html`
+                    <tr data-row-id=${id} class="mdc-data-table__row" >
+                        ${createCheckbox('body', id)}
+                        ${map(data, (value, index) => createBodyCell(index === 0 ? {scope: 'row', id, value} : {value}))}
+                    </tr>
+                `;
+            }
+
+            return html`
+                <tbody class="mdc-data-table__content">
+                    ${map(this.contents, (content, index) => createBodyRow(`${this.id}_row_${index}`, content))}
+                </tbody>
+            `;
+        }
+
         return html`
             <div class="mdc-data-table__table-container">
-                <table class="mdc-data-table__table" aria-label="${aria_label}">
-                    ${this.createHeader()}
-                    ${this.createBody()}
+                <table class="mdc-data-table__table" aria-label="${this.ariaLabel ?? "Data Table"}">
+                    ${createHeader()}
+                    ${createBody()}
                 </table>
             </div>
         `;
