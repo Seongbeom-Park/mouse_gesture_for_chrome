1bcc8714f0f0f938d202804a64270982e29c1875
Seongbeom Park <sparkamita90@gmail.com>
2022-08-09 03:33:21 +0900

    feature: implement top app bar shadow

--- src/options/top_app_bar.js
+++ src/options/top_app_bar.js
@@ -1,4 +1,5 @@
 import { LitElement, html } from 'lit';
+import { ref, createRef } from 'lit/directives/ref.js';
 import { MDCTopAppBar } from '@material/top-app-bar';
 
 class TopAppBar extends LitElement {
@@ -6,8 +7,9 @@ class TopAppBar extends LitElement {
         title: {type: String, attribute: true},
     }
     render () {
+        this.top_app_bar = createRef();
         return html`
-            <header class="mdc-top-app-bar">
+            <header ${ref(this.top_app_bar)} class="mdc-top-app-bar mdc-top-app-bar--fixed">
                 <div class="mdc-top-app-bar__row">
                     <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
                         <span class="mdc-top-app-bar__title">${this.title}</span>
@@ -17,8 +19,7 @@ class TopAppBar extends LitElement {
         `;
     }
     firstUpdated () {
-        this.classList.add('mdc-top-app-bar');
-        this.top_app_bar = new MDCTopAppBar(this);
+        this.top_app_bar = new MDCTopAppBar(this.top_app_bar.value);
     }
     createRenderRoot () {
         return this;
