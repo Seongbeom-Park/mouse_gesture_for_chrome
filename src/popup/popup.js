5d04656b103aeeebae01f69b764bcca03216a516
Seongbeom Park <sparkamita90@gmail.com>
2022-08-19 07:35:58 +0900

    refactor: use local variable, and make actions object as a library

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,60 +3,47 @@ import '@options/styles/options.scss';
 import { LitElement, html } from 'lit';
 
 import { store } from '@common/store';
+import { action_list } from '@content_scripts/actions';
 import { RuleDialog } from '@options/rule_dialog';
 
 class Popup extends LitElement {
     render () {
-        this.rule_dialog = new RuleDialog();
-        this.rule_dialog.fullscreen = true;
-        this.rule_dialog.classList.add('mdc-dialog--open');
-        this.rule_dialog.title = '현재 도메인 이름으로 규칙 추가하기';
-        this.rule_dialog.actions = [
-            'closeTab',
-            'goBack',
-            'goBackOrCloseTab',
-            'goForward',
-            'scrollTop',
-            'scrollBottom',
-            'pageDown',
-            'pageUp',
-            'restore',
-            'keydown',
-            'reload',
-            'nothing',
-        ];
-        this.rule_dialog.default_values = {
+        const rule_dialog = new RuleDialog();
+        rule_dialog.fullscreen = true;
+        rule_dialog.classList.add('mdc-dialog--open');
+        rule_dialog.title = '현재 도메인 이름으로 규칙 추가하기';
+        rule_dialog.actions = action_list;
+        rule_dialog.default_values = {
             domain: '*',
             gesture: '',
             action: '',
             keydown: '',
         }
-        this.rule_dialog.disable_domain = true;
-        this.rule_dialog.onaccept = ({domain, gesture, action, action_details}) => {
+        rule_dialog.disable_domain = true;
+        rule_dialog.onaccept = ({domain, gesture, action, action_details}) => {
             store.addRule(domain, gesture, action, action_details);
             store.sync();
             window.close();
         }
 
-        this.rule_dialog.onClose = () => window.close();
-        this.rule_dialog.onOpenOptions = () => chrome.runtime.openOptionsPage();
+        rule_dialog.onClose = () => window.close();
+        rule_dialog.onOpenOptions = () => chrome.runtime.openOptionsPage();
 
-        return html`
-            ${this.rule_dialog}
-        `;
-    }
-    firstUpdated () {
         const getActiveTab = () => chrome.tabs.query({active: true, lastFocusedWindow: true}).then(tabs => tabs[0]);
         getActiveTab().then((tab) => {
             const url = new URL(tab.url)
             
-            this.rule_dialog.default_values = {
+            rule_dialog.default_values = {
                 domain: url.hostname,
                 gesture: '',
                 action: '',
                 keydown: '',
             }
         });
+
+        return html`
+            ${rule_dialog}
+        `;
     }
     createRenderRoot () {
         return this;
